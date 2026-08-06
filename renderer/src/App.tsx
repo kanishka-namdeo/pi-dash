9d31acef97fb3f980ba4a7a7f47f574c29d16442
kane111 <kanishkanamdeo@hotmail.com>
2026-08-06 13:47:15 +0400

    feat: wrap app with SessionProvider for global session tracking

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -7,6 +7,7 @@ import { WorktreeView } from './components/views/WorktreeView';
 import { CompletedWorkView } from './components/views/CompletedWorkView';
 import { OnboardingFlow } from './components/onboarding/OnboardingFlow';
 import { PiPProvider } from './context/PiPContext';
+import { SessionProvider } from './context/SessionContext';
 import { PiPContainer } from './components/pip/PiPContainer';
 import { MainTerminal } from './components/pip/MainTerminal';
 import { OverlayManager } from './components/pip/OverlayManager';
@@ -35,24 +36,26 @@ function App() {
   }
 
   return (
-    <PiPProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PiPContainer>
-                <MainTerminal />
-                <OverlayManager />
-              </PiPContainer>
-            }
-          />
-          <Route path="/agent/:agentId" element={<TerminalView />} />
-          <Route path="/worktrees" element={<WorktreeView />} />
-          <Route path="/completed/:agentId" element={<CompletedWorkView />} />
-        </Routes>
-      </BrowserRouter>
-    </PiPProvider>
+    <SessionProvider>
+      <PiPProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PiPContainer>
+                  <MainTerminal />
+                  <OverlayManager />
+                </PiPContainer>
+              }
+            />
+            <Route path="/agent/:agentId" element={<AgentDetailView />} />
+            <Route path="/worktrees" element={<WorktreeView />} />
+            <Route path="/completed/:agentId" element={<CompletedWorkView />} />
+          </Routes>
+        </BrowserRouter>
+      </PiPProvider>
+    </SessionProvider>
   );
 }
 export default App;
